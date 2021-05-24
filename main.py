eff75c27719e48de9d5fe028a83649fac38b99f8
mutku <mutku@sabanciuniv.edu>
2021-05-24 19:15:27 +0300

    features in doc done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 import sklearn
 
 # import tensorflow
@@ -12,24 +13,41 @@ from yellowbrick.cluster import SilhouetteVisualizer
 import os
 from collections import Counter, defaultdict, OrderedDict
 
+# A’nın alanı done
+# A’nın genişlik/uzunluk oranı done
+# A’nın tipi (örn. ikon ya da metin) done
+# B’nin alanı done
+# B’nin genişlik/uzunluk oranı done
+# B’nin tipi (örn. ikon ya da metin) done
+# A ile B’nin merkezleri arasındaki çizginin boyu done
+# A ile B’nin merkezleri arasındaki çizginin açısı done
+
+
 dataPath = "./data/kodi-base.csv"
 dataOrg = pd.read_csv(dataPath)
 data = dataOrg.copy()
 data["x"] = data.apply(lambda row: (row.x1 + row.x2) / 2, axis=1)
 data["y"] = data.apply(lambda row: (row.y1 + row.y2) / 2, axis=1)
 data["w/h"] = data.apply(lambda row: row.width / row.height, axis=1)
-data = data.drop(columns=["x1", "y1", "x2", "y2"])
+data["area"] = data.apply(lambda row: (row.x2-row.x1)
+                          * (row.y2 - row.y1), axis=1)
+
+#data = data.drop(columns=["x1", "y1", "x2", "y2"])
 data2 = data
 
 data['key'] = 1
 data2['key'] = 1
-result = pd.merge(data,data2,on='key').drop('key',1)
+result = pd.merge(data, data2, on='key').drop('key', 1)
 result = result[result['_id_x'] != result['_id_y']]
+
+result['center_diff'] = result.apply(lambda row: math.sqrt(
+    pow((row.x_x - row.x_y), 2)+pow((row.y_x - row.y_y), 2)), axis=1)
+result['center_angle'] = result.apply(lambda row: np.rad2deg(
+    np.arctan2((row.y_x - row.y_y), (row.x_x - row.x_y))), axis=1)
+result = result.drop(columns=['x1_x', 'x2_x', 'y1_x', 'y2_x', 'width_x', 'height_x', 'x_x', 'y_x',
+                     'x1_y', 'x2_y', 'y1_y', 'y2_y','width_y', 'height_y', 'x_y', 'y_y'])
+
 print(result)
-quit()
-joined = data.join(data, lsuffix="_x", rsuffix="_y")
-print("Joined:\n", joined)
-print("data:\n" ,data)
 quit()
 numClustersStart = 2
 numClustersEnd = 5
@@ -54,7 +72,8 @@ for n_clusters in range(numClustersStart, numClustersEnd, step):
     if not os.path.exists(path):
         os.mkdir(path)
     startEndPath = str(numClustersStart) + "-" + str(numClustersEnd)
-    visualizer.show(outpath="./" + opath + "/" + str(n_clusters) + ".png")  # TODO
+    visualizer.show(outpath="./" + opath + "/" +
+                    str(n_clusters) + ".png")  # TODO
     plt.cla()
     plt.clf()
     plt.close("all")
