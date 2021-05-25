c4b95d89dbbf87e7014eb4a84a6c2d1a13fd58aa
kagankorkmaz <kagankorkmaz@sabanciuniv.edu>
2021-05-26 02:11:35 +0300

    joined tests

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import matplotlib.cm as cm
 from yellowbrick.cluster import SilhouetteVisualizer
 import os
 from collections import Counter, defaultdict, OrderedDict
+from pprint import pprint
 
 # A’nın alanı done
 # A’nın genişlik/uzunluk oranı done
@@ -29,32 +30,55 @@ data = dataOrg.copy()
 data["x"] = data.apply(lambda row: (row.x1 + row.x2) / 2, axis=1)
 data["y"] = data.apply(lambda row: (row.y1 + row.y2) / 2, axis=1)
 data["w/h"] = data.apply(lambda row: row.width / row.height, axis=1)
-data["area"] = data.apply(lambda row: (row.x2-row.x1)
-                          * (row.y2 - row.y1), axis=1)
+data["area"] = data.apply(lambda row: (row.x2 - row.x1) * (row.y2 - row.y1), axis=1)
 
-#data = data.drop(columns=["x1", "y1", "x2", "y2"])
+# data = data.drop(columns=["x1", "y1", "x2", "y2"])
 data2 = data
 
-data['key'] = 1
-data2['key'] = 1
-result = pd.merge(data, data2, on='key').drop('key', 1)
-result = result[result['_id_x'] != result['_id_y']]
+data["key"] = 1
+data2["key"] = 1
+result = pd.merge(data, data2, on="key").drop("key", 1)
+result = result[result["_id_x"] != result["_id_y"]]
 
-result['center_diff'] = result.apply(lambda row: math.sqrt(
-    pow((row.x_x - row.x_y), 2)+pow((row.y_x - row.y_y), 2)), axis=1)
-result['center_angle'] = result.apply(lambda row: np.rad2deg(
-    np.arctan2((row.y_x - row.y_y), (row.x_x - row.x_y))), axis=1)
-result = result.drop(columns=['x1_x', 'x2_x', 'y1_x', 'y2_x', 'width_x', 'height_x', 'x_x', 'y_x',
-                     'x1_y', 'x2_y', 'y1_y', 'y2_y','width_y', 'height_y', 'x_y', 'y_y'])
+result["center_diff"] = result.apply(
+    lambda row: math.sqrt(pow((row.x_x - row.x_y), 2) + pow((row.y_x - row.y_y), 2)),
+    axis=1,
+)
+result["center_angle"] = result.apply(
+    lambda row: np.rad2deg(np.arctan2((row.y_x - row.y_y), (row.x_x - row.x_y))), axis=1
+)
+result = result.drop(
+    columns=[
+        "x1_x",
+        "x2_x",
+        "y1_x",
+        "y2_x",
+        "width_x",
+        "height_x",
+        # "x_x",
+        # "y_x",
+        "x1_y",
+        "x2_y",
+        "y1_y",
+        "y2_y",
+        "width_y",
+        "height_y",
+        # "x_y",
+        # "y_y",
+    ]
+)
 
-print(result)
-quit()
-numClustersStart = 2
-numClustersEnd = 5
+# print(result)
+# quit()
+numClustersStart = 5
+numClustersEnd = 10
 step = 1
 tol = 1e-04
 maxIter = 300
 distortions = []
+
+data = result.copy()
+data = data.drop(columns=["_id_x", "_id_y"])
 
 for n_clusters in range(numClustersStart, numClustersEnd, step):
     km = KMeans(
@@ -72,8 +96,7 @@ for n_clusters in range(numClustersStart, numClustersEnd, step):
     if not os.path.exists(path):
         os.mkdir(path)
     startEndPath = str(numClustersStart) + "-" + str(numClustersEnd)
-    visualizer.show(outpath="./" + opath + "/" +
-                    str(n_clusters) + ".png")  # TODO
+    visualizer.show(outpath="./" + opath + "/" + str(n_clusters) + ".png")  # TODO
     plt.cla()
     plt.clf()
     plt.close("all")
@@ -89,8 +112,10 @@ for n_clusters in range(numClustersStart, numClustersEnd, step):
     for i in range(n_clusters):
         dictionary[i] = []
     for i in range(len(cluster_labels)):
-        dictionary[cluster_labels[i]].append(dataOrg.loc[i, "_id"])
-    print(dictionary)
+        dictionary[cluster_labels[i]].append(
+            [int(result.iloc[i]["_id_x"]), int(result.iloc[i]["_id_y"])]
+        )
+    pprint(dictionary)
     print("------------------------------\n")
 
 
