6b56f313fcc67f72a96e0a5e66451cbfa7b8c8c6
kagankorkmaz <kagankorkmaz@sabanciuniv.edu>
2021-05-28 00:11:36 +0300

    dbscan printing

--- main.py
+++ main.py
@@ -33,33 +33,58 @@ from collections import Counter, defaultdict, OrderedDict
 dataPath = "./data/kodi-base.csv"
 dataOrg = pd.read_csv(dataPath)
 data = dataOrg.copy()
-print(data)
-#data["x"] = data.apply(lambda row: (row.x1 + row.x2) / 2, axis=1)
-data["y"] = data.apply(lambda row: (row.y1 + row.y2) / 2, axis=1)   # (x1,y)
-#data["w/h"] = data.apply(lambda row: row.width / row.height, axis=1)
+# print(data)
+# data["x"] = data.apply(lambda row: (row.x1 + row.x2) / 2, axis=1)
+data["y"] = data.apply(lambda row: (row.y1 + row.y2) / 2, axis=1)  # (x1,y)
+# data["w/h"] = data.apply(lambda row: row.width / row.height, axis=1)
 # data["area"] = data.apply(lambda row: (row.x2-row.x1)
 #                          * (row.y2 - row.y1), axis=1)
-#data = data.drop(columns=["x1", "y1", "x2", "y2"])
+# data = data.drop(columns=["x1", "y1", "x2", "y2"])
 data2 = data
 
-data['key'] = 1
-data2['key'] = 1
-result = pd.merge(data, data2, on='key').drop('key', 1)
-result = result[result['_id_x'] < result['_id_y']]
+data["key"] = 1
+data2["key"] = 1
+result = pd.merge(data, data2, on="key").drop("key", 1)
+result = result[result["_id_x"] < result["_id_y"]]
 
-result['center_diff'] = result.apply(lambda row: math.sqrt(
-    pow((row.x1_x - row.x1_y), 2)+pow((row.y_x - row.y_y), 2)), axis=1)
-result['center_angle'] = result.apply(lambda row: np.rad2deg(
-    np.arctan2((row.y_x - row.y_y), (row.x1_x - row.x1_y))), axis=1)
-result = result.drop(columns=['x2_x', 'y1_x', 'y2_x', 'width_x', 'height_x',
-                              'x1_y', 'x2_y', 'y1_y', 'y2_y', 'width_y',
-                              'height_y'])
-result['same_row'] = result.apply(lambda row: '1' if abs(
-    row.center_angle) > 177 and abs(row.center_angle) < 183 else '0', axis=1)
-result['same_column'] = result.apply(lambda row: '1' if abs(
-    row.center_angle) > 86 and abs(row.center_angle) < 94 else '0', axis=1)
-result['ndist'] = MinMaxScaler().fit_transform(
-    np.array(result['center_diff']).reshape(-1, 1))
+result["center_diff"] = result.apply(
+    lambda row: math.sqrt(pow((row.x1_x - row.x1_y), 2) + pow((row.y_x - row.y_y), 2)),
+    axis=1,
+)
+result["center_angle"] = result.apply(
+    lambda row: np.rad2deg(np.arctan2((row.y_x - row.y_y), (row.x1_x - row.x1_y))),
+    axis=1,
+)
+result = result.drop(
+    columns=[
+        "x2_x",
+        "y1_x",
+        "y2_x",
+        "width_x",
+        "height_x",
+        "x1_y",
+        "x2_y",
+        "y1_y",
+        "y2_y",
+        "width_y",
+        "height_y",
+    ]
+)
+result["same_row"] = result.apply(
+    lambda row: "1"
+    if abs(row.center_angle) > 177 and abs(row.center_angle) < 183
+    else "0",
+    axis=1,
+)
+result["same_column"] = result.apply(
+    lambda row: "1"
+    if abs(row.center_angle) > 86 and abs(row.center_angle) < 94
+    else "0",
+    axis=1,
+)
+result["ndist"] = MinMaxScaler().fit_transform(
+    np.array(result["center_diff"]).reshape(-1, 1)
+)
 
 ##########################################################################
 # y value is the middle point between y1 and y2 values
@@ -75,24 +100,46 @@ result['ndist'] = MinMaxScaler().fit_transform(
 ############################################################################
 # result = result.drop(columns=['x1_x', 'inside_x', 'y_x', 'inside_y', 'y_y',
 #        'center_diff', 'center_angle', 'same_row', 'same_column', 'ndist'])
-only_id = result.drop(columns=['x1_x', 'inside_x', 'y_x', 'inside_y', 'y_y',
-                               'center_diff', 'center_angle', 'same_row', 'same_column', 'ndist'])
+only_id = result.drop(
+    columns=[
+        "x1_x",
+        "inside_x",
+        "y_x",
+        "inside_y",
+        "y_y",
+        "center_diff",
+        "center_angle",
+        "same_row",
+        "same_column",
+        "ndist",
+    ]
+)
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
 #     print(result)
-result = result.drop(columns=['x1_x', '_id_x', 'y_x', '_id_y', 'y_y',
-                              'center_diff', 'center_angle'])
-print(result)
+result = result.drop(
+    columns=["x1_x", "_id_x", "y_x", "_id_y", "y_y", "center_diff", "center_angle"]
+)
+# print(result)
 
 clustering = DBSCAN(eps=0.5, min_samples=2).fit(result)
-print(clustering.labels_)
-for x in len(set(clustering.labels_)):
-    print("hello")
+# print(clustering.labels_)
 
-only_id['labels'] = clustering.labels_
+only_id["labels"] = clustering.labels_
+clusters = [None] * len(set(clustering.labels_))
+for x in range(len(set(clustering.labels_))):
+    clusters[x] = []
+
+
+for i in range(len(only_id)):
+    clusters[only_id["labels"].values[i]].append([only_id["_id_x"].values[i], only_id["_id_y"].values[i]])
+
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
 #     print(only_id)
 
-
+for i in range(len(clusters)):
+    print(i)
+    pprint(clusters[i])
+    print("--------------------------------")
 quit()
 numClustersStart = 2
 numClustersEnd = 5
@@ -117,8 +164,7 @@ for n_clusters in range(numClustersStart, numClustersEnd, step):
     if not os.path.exists(path):
         os.mkdir(path)
     startEndPath = str(numClustersStart) + "-" + str(numClustersEnd)
-    visualizer.show(outpath="./" + opath + "/" +
-                    str(n_clusters) + ".png")  # TODO
+    visualizer.show(outpath="./" + opath + "/" + str(n_clusters) + ".png")  # TODO
     plt.cla()
     plt.clf()
     plt.close("all")
